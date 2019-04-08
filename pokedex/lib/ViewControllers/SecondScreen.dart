b103cc8fa70b327fe8d9291b1120ec67964124c4
Leandro Bartsch Tha <leandro.tha@livetouch.com.br>
2019-04-08 17:42:38 -0300

    Leitura de CSV e construção de listview parcial

--- pokedex/lib/ViewControllers/SecondScreen.dart
+++ pokedex/lib/ViewControllers/SecondScreen.dart
@@ -1,41 +1,53 @@
-import 'dart:convert';
+import 'package:csv/csv.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:pokedex/Model/JsonModel.dart';
+import 'package:pokedex/Widgets/PokeCell.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class SecondScreen extends StatelessWidget {
+
+  List<List<dynamic>> _fullList;
+  List<PokeCell> _cellList;
+
   @override
   Widget build(BuildContext context) {
+    _loadData();
     return Scaffold(
       appBar: AppBar(
         title: Text("Pokédex"),
         backgroundColor: Colors.red,
       ),
       body: FutureBuilder(
-        future: _fetchPost(),
+        future: _loadData(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Text(snapshot.data.title);
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
+            _loadCells(_fullList);
+            return ListView.builder(
+              itemCount: _cellList.length ?? 0,
+              itemBuilder: (context, index) {
+                return _cellList[index];
+              },
+            );
           }
-
-          return Center(
-            child: CircularProgressIndicator(),
-          );
         }
       ),
     );
   }
 
-  Future<JsonModel> _fetchPost() async {
-    final response = await http.get('https://jsonplaceholder.typicode.com/posts/1');
+  Future<List<List<dynamic>>> _loadData() async {
+    final file = await rootBundle.loadString("Resources/pokemon.csv");
+    List<List<dynamic>> rows = const CsvToListConverter(eol: "\n").convert(file);
 
-    if (response.statusCode == 200) {
-      return JsonModel.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('failed to load request');
+    _fullList = rows;
+
+    return rows;
+  }
+
+  _loadCells(List<List<dynamic>> list) {
+    for (var i = 1; i < list.length; i++) {
+      String name = list[i][1];
+      _cellList.add(PokeCell(name));
     }
   }
+
 }
--- pokedex/lib/Widgets/PokeCell.dart
+++ pokedex/lib/Widgets/PokeCell.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pokedex/Model/PokeCellModel.dart';
 
 class PokeCell extends StatelessWidget {
+
+  final String name;
+
+  PokeCell(this.name);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Pokédex"),
-        backgroundColor: Colors.red
-      ),
-      body: FutureBuilder(
-        future: tbt,
-        builder: (context, data) {
-
-        }
+    return SizedBox(
+      height: 50.0,
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 16.0),
+            child: Image.asset("Thumbnails/" + name.toLowerCase()),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 46.0),
+            child: Text(name),
+          ),
+        ],
       ),
     );
-  }
-
-
-  Future<PokeCellModel> _fetchData() async {
-
   }
 }
